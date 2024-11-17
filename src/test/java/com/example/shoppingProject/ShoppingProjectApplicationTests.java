7a74734132a04ae8f9a9582911e337ffd9fde474
Prajwal23S <yedprajwal@gmail.com>
2024-11-17 23:49:55 +0530

    first commit

---
+++ src/test/java/com/example/shoppingProject/ShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
