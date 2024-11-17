cf9e8242889b7b6fe9c32d8748665d758aa61aaa
Prajwal23S <yedprajwal@gmail.com>
2024-11-17 23:52:56 +0530

    first commit

--- src/main/java/com/example/shoppingProject/ShoppingProjectApplication.java
+++ src/main/java/com/example/shoppingProject/ShoppingProjectApplication.java
@@ -9,6 +9,7 @@ public class ShoppingProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingProjectApplication.class, args);
 		System.out.println("Run Successfull .........");
+		System.out.println("New Line to Test");
 	}
 
 }
