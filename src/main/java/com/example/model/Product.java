5420d4ebffaa10a852c60894b0d905cffa5ca1b2
Prajwal23S <yedprajwal@gmail.com>
2024-11-17 23:36:34 +0530

    first commit

---
+++ src/main/java/com/example/model/Product.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+public class Product {
+    public String ProductName;
+    public int Value;
+    public int tax;
+    public int Quantity;
+    public int ActualValue;
+
+
+}
